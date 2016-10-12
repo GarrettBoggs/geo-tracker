b32ed765547c4d35a711638e94d953e1e0d2154b
Garrett Boggs and Stephen Burden <garrett+stephen@>
2016-10-12 16:17:08 -0700

    add street view

--- js/geo-interface.js
+++ js/geo-interface.js
@@ -4,16 +4,19 @@ var Map = require('./../js/geo.js').mapModule;
 $(document).ready(function(){
   var newMap = new Map();
   var map;
-  $.get('https://maps.googleapis.com/maps/api/geocode/json?address=portland,+or&key=AIzaSyDfb4v-EmKos5paWeelqEGYoOJlvuQr03w').then(function(response) {
+  $.get('https://maps.googleapis.com/maps/api/streetview?size=400x400&location=40.720032,-73.988354&fov=90&heading=235&pitch=10&key=AIzaSyBStk9s-BaIB1CKNciuG_TXCvfeLQJYc6o').then(function(response) {
   console.log(response);
-  newMap.initMap(20, 0);
-
+  newMap.initMap(45, -122);
+  newMap.getStreetView(45, -122);
   $("#lats").submit(function(event){
     event.preventDefault();
     var lat = parseInt($("#latitude").val());
     var long = parseInt($("#longitude").val());
-    console.log(long);
     newMap.initMap(lat, long);
+    // $.get('https://maps.googleapis.com/maps/api/streetview?size=400x400&location=40.720032,-73.988354&fov=90&heading=235&pitch=10&key=AIzaSyBStk9s-BaIB1CKNciuG_TXCvfeLQJYc6o').then(function(response2) {
+    //   console.log(response2);
+    // });
+
   });
       //console.log(JSON.stringify(response));
       // $("#map-display").append(response);
--- js/geo.js
+++ js/geo.js
@@ -7,6 +7,17 @@ Map.prototype.initMap = function (newLat, newLong) {
     center: {lat: newLat , lng: newLong},
     zoom: 8
   });
+
+  Map.prototype.getStreetView = function (newLat, newLong) {
+    var panorama = new google.maps.StreetViewPanorama(
+    document.getElementById('map'), {
+      position: {lat: 37.869260, lng: -122.254811},
+      pov: {heading: 165, pitch: 0},
+      motionTracking: false
+    });
+  };
+
 };
+
 
 exports.mapModule = Map;
