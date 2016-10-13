8fdcf9191d89a90e11307f164f2df96e58464380
Garrett Boggs and Stephen Burden <stephen@>
2016-10-13 16:20:19 -0700

    add random location generation

--- js/geo-interface.js
+++ js/geo-interface.js
@@ -1,19 +1,34 @@
 //var apiKey = require('./../.env').apiKey;
 var Map = require('./../js/geo.js').mapModule;
+var x = document.getElementById("demo");
 
 $(document).ready(function(){
+
+  //console.log(navigator.geolocation.getCurrentPosition(showPosition));
+  //console.log(parseJSON(navigator.geolocation));
   var newMap = new Map();
   var map;
   var lat;
   $.get('https://maps.googleapis.com/maps/api/streetview?size=400x400&location=40.720032,-73.988354&fov=90&heading=235&pitch=10&key=AIzaSyBStk9s-BaIB1CKNciuG_TXCvfeLQJYc6o').then(function(response) {
   //console.log(response);
-     newMap.initMap(45, -122);
+
+    navigator.geolocation.getCurrentPosition(function(position) {
+      //console.log(position.coords.latitude);
+      //console.log(position.coords.longitude);
+    newMap.getStreetView(position.coords.latitude, position.coords.longitude);
+    });
+    //newMap.initMap(49, -122);
 
     $("#lats").submit(function(event){
       event.preventDefault();
       var lat = parseFloat($("#latitude").val());
       var long = parseFloat($("#longitude").val());
       newMap.getStreetView(lat, long);
+    });
+
+    $("#random").click(function(){
+      var random = newMap.randomCords();
+      newMap.getStreetView(random[0], random[1]);
     });
 
   });
@@ -28,5 +43,7 @@ $(document).ready(function(){
       newMap.initMap(lat, long);
       });
     });
+
+
 
 });
--- js/geo.js
+++ js/geo.js
@@ -1,12 +1,14 @@
 function Map() {
 
 }
+var ultraArray = [[40.7127837, -74.0059413], [36.0544445,-112.1401108], [41.8902102, 12.4922309], [51.17888199999999, -1.826215], [38.279277,-81.65017]];
 
   Map.prototype.initMap = function (newLat, newLong) {
     map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: newLat , lng: newLong},
-      zoom: 8
+      zoom: 15
     });
+    console.log("[" + newLat + "," + newLong + "]");
   };
 
 
@@ -18,6 +20,12 @@ function Map() {
       motionTracking: false
     });
   };
-  
+
+  Map.prototype.randomCords = function(){
+    var rando = Math.floor(Math.random()*(ultraArray.length));
+    console.log(rando);
+    return ultraArray[rando];
+  };
+
 exports.mapModule = Map;
 //position: {lat: 37.869260, lng: -122.254811},
